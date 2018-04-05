708d0d56acb6f6eb7b185ecce54d537335b05cd9
Jerry U <jerry@lyshnia.com>
2018-04-06 06:33:34 +0800

    Implemented Birthday

--- app/src/main/java/com/lyshnia/kna/HomePage/Birthdays.kt
+++ app/src/main/java/com/lyshnia/kna/HomePage/Birthdays.kt
@@ -1,21 +1,15 @@
 package com.lyshnia.kna.HomePage
 
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.widget.DrawerLayout
-import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
-import android.webkit.WebView
 import com.lyshnia.kna.DrawerHeader
 import com.lyshnia.kna.DrawerMenuItem
 import com.lyshnia.kna.R
 import com.mindorks.placeholderview.PlaceHolderView
-import kotlinx.android.synthetic.main.activity_birthdays.*
-import kotlinx.android.synthetic.main.activity_tweets.*
 import kotlinx.android.synthetic.main.toolbar_home.*
 
 class Birthdays : AppCompatActivity() {
@@ -28,29 +22,29 @@ class Birthdays : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthdays)
-        toolbarTitle.text = ""
+        toolbarTitle.text = "Birthdays"
 
         mDrawer = findViewById<DrawerLayout>(R.id.drawerLayout);
         mDrawerView = findViewById<PlaceHolderView>(R.id.drawerView);
         mToolbar = findViewById<Toolbar>(R.id.toolbar);
         setupDrawer();
 
-        var toolbar = findViewById<Toolbar>(R.id.toolbar);
+        /*var toolbar = findViewById<Toolbar>(R.id.toolbar);
         if (toolbar != null) {
             // set toolbar object as actionbar
             setSupportActionBar(toolbar);
         }
 
-        /*if (android.os.Build.VERSION.SDK_INT >= 21) {
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
             supportActionBar!!.setBackgroundDrawable(getDrawable(R.drawable.background_toolbar_translucent))
 
         } else {
             supportActionBar!!.setBackgroundDrawable(resources.getDrawable(R.drawable.background_toolbar_translucent))
 
-        }*/
+        }
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setDisplayShowTitleEnabled(false);
+        supportActionBar!!.setDisplayShowTitleEnabled(false);*/
 
     }
 
